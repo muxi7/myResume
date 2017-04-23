cdb70167aaef52e02c9010a95680182653057c5f
muxi7 <1360097606@qq.com>
2017-04-24 00:18:45 +0800

    add 优化

--- js/main.js
+++ js/main.js
@@ -10,28 +10,6 @@ $(window).on('scroll',function(){
   }
 })
 
-
-$('.cover').on("mouseenter",function(e){
-  e.stopPropagation();
-  $(this).css({
-    'background-color':'rgba(255,255,255,0.1)'
-  });
-  $(this).find('h3').stop().animate({
-    'margin-top':150+'px'
-  },300);
-});
-
-$('.cover').on("mouseleave",function(e){
-  e.stopPropagation();
-  $(this).css({
-    'background-color':'rgba(0,0,0,0.25)'
-  });
-  $(this).find('h3').stop().animate({
-    'margin-top':200+'px'
-  },300);
-});
-
-
 $('.nav').on('click','a',function(e){
   e.preventDefault();
   e.stopPropagation();
@@ -43,41 +21,104 @@ $('.nav').on('click','a',function(e){
   $(id).animate({'display':'block'},250);
 })
 
+// $('.cover').on("mouseenter",function(e){
+//   e.stopPropagation();
+//   $(this).css({
+//     'background-color':'rgba(255,255,255,0.1)'
+//   });
+//   $(this).find('h3').stop().animate({
+//     'margin-top':150+'px',
+//   },300);
+// });
+
+// $('.cover').on("mouseleave",function(e){
+//   e.stopPropagation();
+//   $(this).css({
+//     'background-color':'rgba(0,0,0,0.25)'
+//   });
+//   $(this).find('h3').stop().animate({
+//     'margin-top':200+'px'
+//   },300);
+// });
+
+$('.coverLayer').on("mouseenter",function(e){
+  e.stopPropagation();
+  $(this).css({
+    'background-color':getColor(randNum())
+  });
+  $(this).prev('img').animate({
+    'width':'110%',
+    'height':'110%',
+    'left':'-5%',
+    'top':'-5%'
+  },300);
+  $(this).find('h3').stop().animate({
+    'bottom':50+'px',
+  },300);
+  $(this).find('.btn').stop().animate({
+    'bottom':10+'px',
+  },300);
+})
+
+$('.coverLayer').on("mouseleave",function(e){
+  e.stopPropagation();
+  $(this).css({
+    'background-color': 'rgba(0,0,0,0.25)'
+  });
+  $(this).prev('img').animate({
+    'width':'100%',
+    'height':'100%',
+    'left':'0',
+    'top':'0'
+  },300);
+  $(this).find('h3').stop().animate({
+    'bottom':15+'px',
+  },300);
+  $(this).find('.btn').stop().animate({
+    'bottom':-40+'px',
+  },300);
+})
+
+
+
 function randNum(){
-  return Math.round(Math.random()*10);
+  return Math.floor(Math.random()*10);
 }
 
 function getColor(num){
   switch(num){
+    case 0:
+      return 'rgba(22, 160, 133,0.75)';  
+      break;
     case 1:
-      return '';
+      return 'rgba(46, 204, 113,0.75)';  
       break;
     case 2: 
-      return '';
+      return 'rgba(52, 152, 219,0.75)'; 
       break;
     case 3:
-      return '';
+      return 'rgba(241, 196, 15,0.75'; 
       break;
     case 4: 
-      return '';
+      return 'rgba(231, 76, 60,0.75)'; 
       break;
     case 5:
-      return '';
+      return 'rgba(211, 84, 0,0.75)'; 
       break;
     case 6: 
-      return '';
+      return 'rgba(59,178,75,0.75)'; 
       break;
     case 7:
-      return '';
+      return 'rgba(82,168,153,0.75)'; 
       break;
     case 8: 
-      return '';
+      return 'rgba(26,188,156,0.75)'; 
       break;
     case 9:
-      return '';
+      return 'rgba(238,157,168,0.75)'; 
       break;
-    case 10: 
-      return '';
+    default: 
+      return 'rgba(243,156,18,0.75)'; //ee9da8
       break;
   }
 }
